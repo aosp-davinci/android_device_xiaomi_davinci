03dadec71151293128552b099f8787b6c27d2415
Chirayu Desai <chirayudesai1@gmail.com>
2021-04-15 02:09:34 +0530

    davinci: rro_overlays: Move CarrierConfig to RRO
    Change-Id: Ia57acd244608848f70fdcc30d5694c919db60671

--- device.mk
+++ device.mk
@@ -469,6 +469,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
 
+# Overlays-RRO
+PRODUCT_PACKAGES += \
+    CarrierConfigOverlay \
+    WifiResCommon
+
 # WiFi
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service \
@@ -478,7 +483,6 @@ PRODUCT_PACKAGES += \
     libwpa_client \
     vendor.qti.hardware.wifi.hostapd@1.2.vendor \
     vendor.qti.hardware.wifi.supplicant@2.2.vendor \
-    WifiResCommon \
     wpa_supplicant \
     wpa_supplicant.conf
 
