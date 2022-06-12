c913094e491145bc02096343d6fb529fe6f780cf
raysenlau <rayen@live.cn>
2022-06-12 14:09:35 +0800

    Revert "davinci: Go away Megvii"
    This reverts commit dc55f3a198ed6d3f46c9c6c95966f66433ff3285.

--- device.mk
+++ device.mk
@@ -157,10 +157,6 @@ PRODUCT_PACKAGES += \
     vendor.xiaomi.hardware.motor@1.0.vendor
 
 PRODUCT_PACKAGES += \
-    libMegviiFacepp-0.5.2 \
-    libmegface
-
-PRODUCT_PACKAGES += \
     GCamGOPrebuilt
 
 # Component overrides
